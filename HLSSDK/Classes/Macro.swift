680ae5b8ea269d7753329004a87f5190a35c56d2
fynmm <974800149@qq.com>
2019-01-22 18:36:20 +0800

    11

---
+++ HLSSDK/Classes/Macro.swift
@@ -0,0 +1,11 @@
+//
+//  Macro.swift
+//  HLSSDK
+//
+//  Created by HeartlessSoy on 2019/1/22.
+//
+
+import Foundation
+
+public let HLS_SCREEN_WIDTH = UIScreen.main.bounds.width
+public let HLS_SCREEN_HEIGHT = UIScreen.main.bounds.height
---
+++ Macro.swift
@@ -0,0 +1,8 @@
+//
+//  Macro.swift
+//  HLSSDK
+//
+//  Created by HeartlessSoy on 2019/1/22.
+//
+
+import Foundation
